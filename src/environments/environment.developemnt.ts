c7720c4fcf40178193fadb74cb57a6ff5b229b88
kajalblog <kajalparchandee.com>
2026-04-25 20:18:43 +0530

    env changes

---
+++ src/environments/environment.developemnt.ts
@@ -0,0 +1,5 @@
+export const url = {
+    // apiUrl:"http://localhost:4000/api",
+    apiUrl: 'https://school-backend-1-9syr.onrender.com/api',
+    production: false
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    // apiUrl:"http://localhost:4000/api",
+    apiUrl: 'https://school-backend-1-9syr.onrender.com/api',
+    production: true
+}
