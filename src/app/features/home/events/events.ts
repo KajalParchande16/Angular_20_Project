2b963908f469abac9c94b641cc1d6ee72bbc4fc8
kajalblog <kajalparchandee.com>
2026-04-29 22:02:32 +0530

    home page design changes

--- src/app/features/home/events/events.ts
+++ src/app/features/home/events/events.ts
@@ -1,26 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Api } from '../../../services/api';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-
+import { register } from 'swiper/element/bundle';
 @Component({
   selector: 'app-events',
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './events.html',
-  styleUrl: './events.scss'
+  styleUrl: './events.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Events implements OnInit {
-  events: any=[];
-  constructor(private apiService:Api){
+  events: any = [];
+  @ViewChild('swiperRef') swiperRef!: ElementRef;
+  constructor(private apiService: Api) {
 
   }
   ngOnInit(): void {
     this.getEvent();
+    register();
   }
+  ngAfterViewInit() {
+
+    setTimeout(() => {
+
+      Object.assign(this.swiperRef.nativeElement, {
+
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false
+        },
+
+        breakpoints: {
+          576: { slidesPerView: 1 },
+          768: { slidesPerView: 2 },
+          1200: { slidesPerView: 3 }
+        }
+
+      });
+
+      this.swiperRef.nativeElement.initialize();
+
+    }, 300);
+  }
+
   getEvent() {
     this.apiService.getEvent().subscribe({
       next: (res: any) => {
-        this.events=res.data;
+        this.events = res.data;
         console.log(this.events);
 
       }, error(error: any) {
